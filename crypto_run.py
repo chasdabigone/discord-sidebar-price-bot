4441df44de7b473a7060127dc3c7e484ed3e9216
chas s <chasdabigone@gmail.com>
2021-09-15 13:50:43 -0500

    exception does not stop bot updates

--- crypto_run.py
+++ crypto_run.py
@@ -16,11 +16,12 @@ def get_blockchain():
     global decimals
     global quipu
     global updateCounter
-    global kUSDpeg
+    global pairPeg
     global pairPrice
     global harbingerPrice
     global config
 
+    
     #fetch harbinger oracle price
     harbinger = pytezos.using(config['tezosNode'])
     harbinger = harbinger.contract('KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9')
@@ -44,16 +45,19 @@ def get_blockchain():
     pairRatio = (XTZamt / math.pow(10, 6)) / (pairAmt / math.pow(10, decimals))
     pairPrice = harbingerPrice * pairRatio
     pairPeg = (pairPrice - 1) * 100
+     
     
 
 def main(ticker: str,
          verbose: bool = False) -> None:
+    import argparse
     import math
     import json, yaml
     import discord
     import asyncio
     from pytezos import pytezos
     global pairSymbol
+    global pairPeg
     global quipu
     global decimals
     global harbingerPrice
@@ -73,52 +77,61 @@ def main(ticker: str,
     # 2. Get Initial Blockchain Data
 
 
-    
-    #fetch quipuswap contract data
-    quipu = pytezos.using(config['tezosNode'])
-    quipu = quipu.contract(config['contractAddress'])
+    try:
+        #fetch quipuswap contract data
+        quipu = pytezos.using(config['tezosNode'])
+        quipu = quipu.contract(config['contractAddress'])
 
-    #fetch pair data
-    pairAddress = quipu.storage['storage']['token_address']()
-    pairContract = pytezos.using(config['tezosNode'])
-    pairContract = pairContract.contract(pairAddress)
+        #fetch pair data
+        pairAddress = quipu.storage['storage']['token_address']()
+        pairContract = pytezos.using(config['tezosNode'])
+        pairContract = pairContract.contract(pairAddress)
 
-    #find symbol of pair
-    pairSymbol = pairContract.storage['token_metadata'][0]()[1]['symbol'].decode()
+        #find symbol of pair
+        pairSymbol = pairContract.storage['token_metadata'][0]()[1]['symbol'].decode()
 
-    #find mantissa of pair
-    decimals_str = pairContract.storage['token_metadata'][0]()[1]['decimals']
-    #set mantissa variable
-    decimals = int(decimals_str)
+        #find mantissa of pair
+        decimals_str = pairContract.storage['token_metadata'][0]()[1]['decimals']
+        #set mantissa variable
+        decimals = int(decimals_str)
+    except:
+        pass
+        main(ticker=args.ticker,
+         verbose=args.verbose)
 
     # 3. Connect w the bot
-    client = discord.Client()
-    numUnit = len(config['priceUnit'])#this isn't implemented right now
+    try:
+        client = discord.Client()
+        numUnit = len(config['priceUnit'])#this isn't implemented right now
 
-    async def send_update(priceList, unit, numDecimalPlace=None):
-        if numDecimalPlace == 0:
-            numDecimalPlace = None # round(2.3, 0) -> 2.0 but we don't want ".0"
+        async def send_update(priceList, unit, numDecimalPlace=None):
+            if numDecimalPlace == 0:
+                numDecimalPlace = None # round(2.3, 0) -> 2.0 but we don't want ".0"
 
 
-        #set name and status
-        if config['stablecoinPeg'] == True:
+            #set name and status
+            if config['stablecoinPeg'] == True:
             
-            nickname = pairSymbol + ' Peg' + ' ' + str(round(pairPeg, 2)) + '%'
+                nickname = pairSymbol + ' Peg' + ' ' + str(round(pairPeg, 2)) + '%'
 
-            if updateCounter == 1: #cycling between showing pair/XTZ
-                status = pairSymbol + ' Price' + ' ' + '$' + str(round(pairPrice, numDecimalPlace))
+                if updateCounter == 1: #cycling between showing pair/XTZ
+                    status = pairSymbol + ' Price' + ' ' + '$' + str(round(pairPrice, numDecimalPlace))
+                else:
+                    status = 'XTZ Price' + ' ' + '$' + str(round(harbingerPrice, 2))
             else:
-                status = 'XTZ Price' + ' ' + '$' + str(round(harbingerPrice, 2))
-        else:
-            nickname = pairSymbol + ' ' + '$' + str(round(pairPrice, numDecimalPlace))
-            status = 'XTZ ' + '$' + str(round(harbingerPrice, 2))
+                nickname = pairSymbol + ' ' + '$' + str(round(pairPrice, numDecimalPlace))
+                status = 'XTZ ' + '$' + str(round(harbingerPrice, 2))
 
 
-        await client.wait_until_ready()
-        await client.get_guild(config['guildId']).me.edit(nick=nickname)
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+            await client.wait_until_ready()
+            await client.get_guild(config['guildId']).me.edit(nick=nickname)
+            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
                                                                name=status))
-        await asyncio.sleep(config['updateFreq'] / numUnit) # in seconds
+            await asyncio.sleep(config['updateFreq'] / numUnit) # in seconds
+    except:
+        pass
+        main(ticker=args.ticker,
+         verbose=args.verbose)
 
     @client.event
     async def on_ready():
@@ -126,25 +139,31 @@ def main(ticker: str,
         When discord client is ready
         """
         while True:
-            # 4. Fetch contract prices
+            try:
+                # 4. Fetch contract prices
         
-            priceList = get_blockchain()
+                priceList = get_blockchain()
             
             
             
         
-            # 5. Feed it to the bot
-            # max. 3 priceUnit (tried to avoid using for loop)
-            await send_update(priceList, config['priceUnit'][0].lower(), config['decimalPlace'][0])
-            if len(config['priceUnit']) >= 2:
-                await send_update(priceList, config['priceUnit'][1].lower(), config['decimalPlace'][1])
-            if len(config['priceUnit']) >= 3:
-                await send_update(priceList, config['priceUnit'][2].lower(), config['decimalPlace'][2])
-
-    client.run(config['discordBotKey'])
+                # 5. Feed it to the bot
+                # max. 3 priceUnit (tried to avoid using for loop)
+                await send_update(priceList, config['priceUnit'][0].lower(), config['decimalPlace'][0])
+                if len(config['priceUnit']) >= 2:
+                    await send_update(priceList, config['priceUnit'][1].lower(), config['decimalPlace'][1])
+                if len(config['priceUnit']) >= 3:
+                    await send_update(priceList, config['priceUnit'][2].lower(), config['decimalPlace'][2])
+            except:
+                pass
+    try:
+        client.run(config['discordBotKey'])
+    except:
+        pass
 
 if __name__ == '__main__':
     import argparse
+    global args
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--ticker',
